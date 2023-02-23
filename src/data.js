e59023baaa763c0892fd997d4a2e71e5ca69444a
Saiken Ho <saikenho@Saikens-MacBook-Air.local>
2023-02-23 06:59:48 -0500

    added product images

---
+++ src/data.js
@@ -0,0 +1,53 @@
+// create an object to return sample products
+ 
+const data={ 
+    products: [
+        {
+            name:' Nike Slim shirt', 
+            slug:'nike-slim-shirt', 
+            category:'Shirts', 
+            image:'/images/p1.jpg', 
+            price:120, 
+            countInStock:10, 
+            brand:'Nike', 
+            rating:4.5, 
+            numReviews:10, 
+            description:'Ok quality poroduct' 
+        },     {
+            name:' Adidas Slim shirt', 
+            slug:'adidas-slim-shirt', 
+            category:'Shirts', 
+            image:'/images/p2.jpg', 
+            price:110, 
+            countInStock:50, 
+            brand:'Adidas', 
+            rating:4.0, 
+            numReviews:10, 
+            description:'medium quality ' 
+        },     {
+            name:' Paint Slim shirt', 
+            slug:'Paint-slim-shirt', 
+            category:'Shirts', 
+            image:'/images/p3.jpg', 
+            price:120, 
+            countInStock:10, 
+            brand:'Paint', 
+            rating:4.5, 
+            numReviews:10, 
+            description:'high quality poroduct' 
+        },     {
+            name:' UnderArmour Slim shirt', 
+            slug:'AM-slim-shirt', 
+            category:'Shirts', 
+            image:'/images/p3.jpg', 
+            price:120, 
+            countInStock:10, 
+            brand:'AM', 
+            rating:4.5, 
+            numReviews:10, 
+            description:'high quality poroduct' 
+        },
+    ],
+}
+
+export default data  ;
