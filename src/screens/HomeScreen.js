15f53480d8c667493da9712e4de82d9a39942cc8
Saiken Ho <saikenho@saikens-air.lan>
2023-02-26 10:23:48 -0500

    added product screen information

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,28 +1,69 @@
-import { Link } from "react-router-dom"
-import data from "../data"
+import { useEffect, useReducer, useState } from "react";
 
-function HomeScreen (){ 
-    return (<div> 
-     <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name}></img>
-              </a>
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Product from "../components/Product";
+import axios from "axios";
+import logger from "use-reducer-logger";
+import { Helmet } from "react-helmet-async";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    // set the loading to true so you can show a loading box ;
 
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  {" "}
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong> ${product.price}</strong>
-                </p>
-                <button>Add to Cart</button>
-              </div>
-            </div>
-          ))}
-        </div></div>)
+    case "FETCH_SUCESS":
+      return { ...state, products: action.payload, loading: false };
+
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+function HomeScreen() {
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    loading: true,
+    error: "",
+    products: [],
+  });
+  // const [products,setProducts]=useState([]);
+  //use effect
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products/");
+        dispatch({ type: "FETCH_SUCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
+
+      // setProducts(result.data); // gets the data from the backend
+    };
+    fetchData(); //executes fetch data
+  }, []);
+  return (
+    <div>
+    <Helmet><title>Amazona</title></Helmet>
+      <h1>Featured Products</h1>
+      <div className="products">
+        {loading ? (
+          <div> Loading</div>
+        ) : error ? (
+          <div> {error} </div>
+        ) : (
+          <Row>
+            {products.map((product) => (
+              <Col key={product.slug} sm={6} md={3} lg={4} className="mb-3">
+                <Product product={product}></Product>
+              </Col>
+            ))}
+          </Row>
+        )}
+      </div>
+    </div>
+  );
 }
-export default HomeScreen
+export default HomeScreen;
--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,13 +1,119 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import { useReducer, useEffect } from "react";
+import axios from "axios";
+import Col from "react-bootstrap/esm/Col";
+import Row from "react-bootstrap/esm/Row";
+import ListGroup from "react-bootstrap/ListGroup";
+import ListGroupItem from "react-bootstrap/esm/ListGroupItem";
+import Badge from "react-bootstrap/Badge";
+import Card from "react-bootstrap/Card";
+import Rating from "../components/Rating";
+import Button from "react-bootstrap/esm/Button";
+import { Helmet } from "react-helmet-async";
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    // set the loading to true so you can show a loading box ;
 
-function ProductScreen () { 
-const params=   useParams() ;
-const {slug}=params; 
-return( <div> 
-<h1>{slug}</h1>
-</div>)
+    case "FETCH_SUCESS":
+      return { ...state, product: action.payload, loading: false };
 
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+function ProductScreen() {
+  const params = useParams();
+  const { slug } = params;
+  const [{ loading, error, product }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: "",
+    product: [], // show only one product
+  });
+  // const [products,setProducts]=useState([]);
+  //use effect
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get(`/api/products/slug/${slug}`);
+        dispatch({ type: "FETCH_SUCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: err.message });
+      }
 
+      // setProducts(result.data); // gets the data from the backend
+    };
+    fetchData(); //executes fetch data
+  }, [slug]);
+  //   use slug so when there is a change there would be an uodated in the website
+  return loading ? (
+    <div>Loading....</div>
+  ) : error ? (
+    <div>{error}</div>
+  ) : (
+    <div>
+      <Row>
+        <Col md={6}>
+          <img className="img-large" src={product.image} alt={product.name} />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+            <Helmet> <title>{product.name}</title></Helmet>
+              <h1>{product.name}</h1>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating rating={product.rating} numReviews={product.numReviews}>
+                {" "}
+              </Rating>
+            </ListGroup.Item>
+            <ListGroupItem>Price: ${product.price}</ListGroupItem>
+            <ListGroupItem>
+              Description: <p>{product.description}</p>
+            </ListGroupItem>
+          </ListGroup>
+        </Col>
+        {/* create cart */}
+        <Col md={3}>
+          <Card>
+            <Card.Body>
+              <ListGroup variant="flush ">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>${product.price}</Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? (
+                        <Badge bg="success"> In Stock</Badge>
+                      ) : (
+                        <Badge bg="danger"> Out of Stock</Badge>
+                      )}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                {product.countInStock > 0 && (
+                  <ListGroupItem>
+                    <div className="d-grid">
+                      <Button variant="primary">Add to Cart </Button>
+                    </div>
+                  </ListGroupItem>
+                )}
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
-export default ProductScreen
+export default ProductScreen;
